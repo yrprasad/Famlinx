b9e40fde4d597630c43504e7a1aab335958b97ba
Raghavendra Yelisetty <raghavendra.pra.yelisetty@hpe.com>
2018-06-29 20:37:29 +0530

    Slow loading of Buy Screen

--- app/src/main/java/com/nglinx/pulse/activity/BuyNowActivity.java
+++ app/src/main/java/com/nglinx/pulse/activity/BuyNowActivity.java
@@ -1,10 +1,12 @@
 package com.nglinx.pulse.activity;
 
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -29,12 +31,12 @@ import java.util.ArrayList;
 
 public class BuyNowActivity extends AbstractActivity {
 
-    //    HorizontalView hv_devices;
-//    DeviceTypesAdapter deviceTypesAdapter;
+    HorizontalView hv_devices;
+    DeviceTypesAdapter deviceTypesAdapter;
     ArrayList<DeviceTypesModel> deviceTypesList;
     ArrayList<DeviceCartModel> devicesCart;
 
-//    DeviceTypeClickListener deviceTypeClickListener;
+    DeviceTypeClickListener deviceTypeClickListener;
 
     TextView tv_size_1;
     TextView tv_size_2;
@@ -48,15 +50,17 @@ public class BuyNowActivity extends AbstractActivity {
     TextView tv_care_2;
     TextView tv_care_3;
 
-    ImageView img_mini_device_type;
-    ImageView img_mini_device_type_1;
-    ImageView img_mini_device_type_2;
-    ImageView img_mini_device_type_3;
-    ImageView img_mini_device_type_4;
+//    ImageView img_mini_device_type;
+//    ImageView img_mini_device_type_1;
+//    ImageView img_mini_device_type_2;
+//    ImageView img_mini_device_type_3;
+//    ImageView img_mini_device_type_4;
 
     TextView tv_device_count_minus;
     NumberPicker tv_device_count_value;
     TextView tv_device_count_plus;
+
+    TextView tv_selectedDevice;
 
     NumberPicker et_rental_days;
 
@@ -77,11 +81,11 @@ public class BuyNowActivity extends AbstractActivity {
 
     protected void initializeIcons() {
         deviceTypesList = new ArrayList<>();
-//        deviceTypesAdapter = new DeviceTypesAdapter(getApplicationContext(), deviceTypesList);
-//        hv_devices = (HorizontalView) findViewById(R.id.hv_devices);
-//        hv_devices.setAdapter(deviceTypesAdapter);
-//        deviceTypeClickListener = new DeviceTypeClickListener();
-//        hv_devices.setOnItemClickListener(deviceTypeClickListener);
+        deviceTypesAdapter = new DeviceTypesAdapter(getApplicationContext(), deviceTypesList);
+        hv_devices = (HorizontalView) findViewById(R.id.hv_devices);
+        hv_devices.setAdapter(deviceTypesAdapter);
+        deviceTypeClickListener = new DeviceTypeClickListener();
+        hv_devices.setOnItemClickListener(deviceTypeClickListener);
 
         tv_size_1 = (TextView) findViewById(R.id.tv_size_1);
         tv_size_2 = (TextView) findViewById(R.id.tv_size_2);
@@ -95,17 +99,19 @@ public class BuyNowActivity extends AbstractActivity {
         tv_care_2 = (TextView) findViewById(R.id.tv_care_2);
         tv_care_3 = (TextView) findViewById(R.id.tv_care_3);
 
-        img_mini_device_type = (ImageView) findViewById(R.id.img_mini_device_type);
-        img_mini_device_type_1 = (ImageView) findViewById(R.id.img_mini_device_type_1);
-        img_mini_device_type_2 = (ImageView) findViewById(R.id.img_mini_device_type_2);
-        img_mini_device_type_3 = (ImageView) findViewById(R.id.img_mini_device_type_3);
-        img_mini_device_type_4 = (ImageView) findViewById(R.id.img_mini_device_type_4);
+//        img_mini_device_type = (ImageView) findViewById(R.id.img_mini_device_type);
+//        img_mini_device_type_1 = (ImageView) findViewById(R.id.img_mini_device_type_1);
+//        img_mini_device_type_2 = (ImageView) findViewById(R.id.img_mini_device_type_2);
+//        img_mini_device_type_3 = (ImageView) findViewById(R.id.img_mini_device_type_3);
+//        img_mini_device_type_4 = (ImageView) findViewById(R.id.img_mini_device_type_4);
 
 //        tv_device_count_minus = (TextView) findViewById(R.id.tv_device_count_minus);
         tv_device_count_value = (NumberPicker) findViewById(R.id.tv_device_count_value);
         tv_device_count_value.setMinValue(0);
         tv_device_count_value.setMaxValue(20);
         tv_device_count_value.setWrapSelectorWheel(false);
+
+        tv_selectedDevice = (TextView) findViewById(R.id.tv_selectedDevice);
 
 //        tv_device_count_plus = (TextView) findViewById(R.id.tv_device_count_plus);
 
@@ -125,7 +131,7 @@ public class BuyNowActivity extends AbstractActivity {
         if (ds.getDeviceTypesList() != null) {
             deviceTypesList.clear();
             deviceTypesList.addAll(ds.getDeviceTypesList());
-//            deviceTypesAdapter.notifyDataSetChanged();
+            deviceTypesAdapter.notifyDataSetChanged();
             return;
         }
 
@@ -138,7 +144,7 @@ public class BuyNowActivity extends AbstractActivity {
                 ApplicationUtils.updateDefaultValueIfDeviceDescIsNull(models);
                 deviceTypesList.addAll(ApplicationUtils.getNonSensorDeviceTypesForBuy(models));
                 deviceTypesList.addAll(ApplicationUtils.getNonSensorDeviceTypesForRent(models));
-//                deviceTypesAdapter.notifyDataSetChanged();
+                deviceTypesAdapter.notifyDataSetChanged();
 
                 //Initialize the Cart
                 ds.setDeviceTypesList(deviceTypesList);
@@ -186,31 +192,33 @@ public class BuyNowActivity extends AbstractActivity {
         tv_care_2.setText(ApplicationConstants.BUY_NOW_DESC_BULLET_PREFIX + selectedMember.getCare().get(1));
         tv_care_3.setText(ApplicationConstants.BUY_NOW_DESC_BULLET_PREFIX + selectedMember.getCare().get(2));
 
-        if (selectedMember.getType().equals(DeviceType.A9)) {
-            img_mini_device_type.setImageResource(R.drawable.a9);
-            img_mini_device_type_1.setImageResource(R.drawable.a9);
-            img_mini_device_type_2.setImageResource(R.drawable.a9);
-            img_mini_device_type_3.setImageResource(R.drawable.a9);
-            img_mini_device_type_4.setImageResource(R.drawable.a9);
-        } else if (selectedMember.getType().equals(DeviceType.V7)) {
-            img_mini_device_type.setImageResource(R.drawable.v7);
-            img_mini_device_type_1.setImageResource(R.drawable.v7);
-            img_mini_device_type_2.setImageResource(R.drawable.v7);
-            img_mini_device_type_3.setImageResource(R.drawable.v7);
-            img_mini_device_type_4.setImageResource(R.drawable.v7);
-        } else if (selectedMember.getType().equals(DeviceType.V16)) {
-            img_mini_device_type.setImageResource(R.drawable.v16);
-            img_mini_device_type_1.setImageResource(R.drawable.v16);
-            img_mini_device_type_2.setImageResource(R.drawable.v16);
-            img_mini_device_type_3.setImageResource(R.drawable.v16);
-            img_mini_device_type_3.setImageResource(R.drawable.v16);
-        } else if (selectedMember.getType().equals(DeviceType.MQTT)) {
-            img_mini_device_type.setImageResource(R.drawable.accept);
-            img_mini_device_type_1.setImageResource(R.drawable.accept);
-            img_mini_device_type_2.setImageResource(R.drawable.accept);
-            img_mini_device_type_3.setImageResource(R.drawable.accept);
-            img_mini_device_type_4.setImageResource(R.drawable.accept);
-        }
+        tv_selectedDevice.setText(selectedMember.getName() + " " + selectedMember.getOrderType().toString());
+
+//        if (selectedMember.getType().equals(DeviceType.A9)) {
+//            img_mini_device_type.setImageResource(R.drawable.a9);
+//            img_mini_device_type_1.setImageResource(R.drawable.a9);
+//            img_mini_device_type_2.setImageResource(R.drawable.a9);
+//            img_mini_device_type_3.setImageResource(R.drawable.a9);
+//            img_mini_device_type_4.setImageResource(R.drawable.a9);
+//        } else if (selectedMember.getType().equals(DeviceType.V7)) {
+//            img_mini_device_type.setImageResource(R.drawable.v7);
+//            img_mini_device_type_1.setImageResource(R.drawable.v7);
+//            img_mini_device_type_2.setImageResource(R.drawable.v7);
+//            img_mini_device_type_3.setImageResource(R.drawable.v7);
+//            img_mini_device_type_4.setImageResource(R.drawable.v7);
+//        } else if (selectedMember.getType().equals(DeviceType.V16)) {
+//            img_mini_device_type.setImageResource(R.drawable.v16);
+//            img_mini_device_type_1.setImageResource(R.drawable.v16);
+//            img_mini_device_type_2.setImageResource(R.drawable.v16);
+//            img_mini_device_type_3.setImageResource(R.drawable.v16);
+//            img_mini_device_type_3.setImageResource(R.drawable.v16);
+//        } else if (selectedMember.getType().equals(DeviceType.MQTT)) {
+//            img_mini_device_type.setImageResource(R.drawable.accept);
+//            img_mini_device_type_1.setImageResource(R.drawable.accept);
+//            img_mini_device_type_2.setImageResource(R.drawable.accept);
+//            img_mini_device_type_3.setImageResource(R.drawable.accept);
+//            img_mini_device_type_4.setImageResource(R.drawable.accept);
+//        }
 
         tv_device_count_value.setValue(0);
 
@@ -237,7 +245,7 @@ public class BuyNowActivity extends AbstractActivity {
 
         int rentalDays = et_rental_days.getValue();
 
-        if (selectedMember.getOrderType() == DeviceOrderType.RENT) {
+        if(selectedMember.getOrderType() == DeviceOrderType.RENT) {
             if (rentalDays <= 0) {
                 Toast.makeText(this, "Rental days cannot be zero", Toast.LENGTH_SHORT).show();
                 return;
@@ -262,42 +270,4 @@ public class BuyNowActivity extends AbstractActivity {
         Intent intent = new Intent(getApplication(), CartActivity.class);
         startActivity(intent);
     }
-
-
-    public void onA9BasicBuyClick(View v) {
-        selectedMember = ApplicationUtils.getDeviceByType(deviceTypesList, DeviceType.A9);
-
-        if (selectedMember != null)
-            displaySelectedDeviceDetails(selectedMember);
-        else
-            Toast.makeText(this, "Unable to get the info", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onV7AdvancedBuyClick(View v) {
-        selectedMember = ApplicationUtils.getDeviceByType(deviceTypesList, DeviceType.V7);
-
-        if (selectedMember != null)
-            displaySelectedDeviceDetails(selectedMember);
-        else
-            Toast.makeText(this, "Unable to get the info", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onA9BasicRentClick(View v) {
-        selectedMember = ApplicationUtils.getDeviceByType(deviceTypesList, DeviceType.A9);
-
-        if (selectedMember != null)
-            displaySelectedDeviceDetails(selectedMember);
-        else
-            Toast.makeText(this, "Unable to get the info", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onV7AdvancedRentClick(View v) {
-        selectedMember = ApplicationUtils.getDeviceByType(deviceTypesList, DeviceType.V7);
-
-        if (selectedMember != null)
-            displaySelectedDeviceDetails(selectedMember);
-        else
-            Toast.makeText(this, "Unable to get the info", Toast.LENGTH_SHORT).show();
-    }
-
 }
